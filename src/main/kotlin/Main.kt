329c67037d80f07f831d2f7487c1780e44d55f7f
Ralph Ryan Escabarte <rtescabarte1@up.edu.ph>
2026-08-24 00:37:18 +0800

    feat: print team member names on startup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,6 @@
 fun main(args: Array<String>) {
     println("Hello from Team Peanut Butterbonia!")
+    println("Members:")
+    println("Ralph Ryan T. Escabarte")
+    println("John Matthew N. Fallarme")
 }
