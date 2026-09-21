f234581d58d0e53d2c9f5ef93aff48e44b77f950
John Matthew Fallarme <jnfallarme@up.edu.ph>
2026-09-21 22:08:50 +0800

    feat: add parser helper functions and error reporting, parse literal expressions in primary,  and parse left-associative term expressions

---
+++ src/main/kotlin/Parser.kt
@@ -0,0 +1,65 @@
+class Parser(private val tokens: List<Token>) {
+    private var current = 0
+
+    private fun isAtEnd(): Boolean = peek().type == TokenType.EOF
+
+    private fun peek(): Token = tokens[current]
+
+    private fun previous(): Token = tokens[current - 1]
+
+    private fun advance(): Token {
+        if (!isAtEnd()) current++
+        return previous()
+    }
+
+    private fun check(type: TokenType): Boolean {
+        if (isAtEnd()) return false
+        return peek().type == type
+    }
+
+    private fun match(vararg types: TokenType): Boolean {
+        for (type in types) {
+            if (check(type)) {
+                advance()
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun consume(type: TokenType, message: String): Token {
+        if (check(type)) return advance()
+        throw error(peek(), message)
+    }
+
+    private fun error(token: Token, message: String): ParseError {
+        val where = if (token.type == TokenType.EOF) "end" else "'${token.lexeme}'"
+        ErrorReporter.error(token.line, "Error at $where: $message")
+        return ParseError()
+    }
+
+    class ParseError : RuntimeException()
+
+    private fun primary(): Expr {
+        if (match(TokenType.NUMBER, TokenType.STRING)) {
+            return Expr.Literal(previous().literal)
+        }
+        if (match(TokenType.ACTIVE)) return Expr.Literal(true)
+        if (match(TokenType.INACTIVE)) return Expr.Literal(false)
+        if (match(TokenType.VOID)) return Expr.Literal(null)
+
+        throw error(peek(), "Expect expression.")
+    }
+
+    private fun term(): Expr {
+        var expr = primary()
+        while (match(TokenType.MINUS, TokenType.PLUS)) {
+            val operator = previous()
+            val right = primary()
+            expr = Expr.Binary(expr, operator, right)
+        }
+        return expr
+    }
+
+    fun parse(): Expr = term()
+}
